b22f674d66a123f72474a7a8c427574809a7bf6f
andrewarosario <andrew.arosario@gmail.com>
2023-08-29 21:35:33 -0300

    feat: integrate routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HelloWorldComponent,
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('modules/products').then((m) => m.ProductsModule),
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('modules/orders').then((m) => m.OrdersModule),
+  },
+];
