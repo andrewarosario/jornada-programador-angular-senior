1f580ed6bd2debe6491d77de30c333333e21993f
andrewarosario <andrew.arosario@gmail.com>
2023-08-29 21:41:05 -0300

    feat: add ui modules

--- modules/orders/src/lib/orders.module.ts
+++ modules/orders/src/lib/orders.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiComponent } from 'modules/shared/ui';
 import { ordersRoutes } from './lib.routes';
 import { OrderListComponent } from './order-list/order-list.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(ordersRoutes)],
+  imports: [CommonModule, RouterModule.forChild(ordersRoutes), UiComponent],
   declarations: [OrderListComponent],
 })
 export class OrdersModule {}
--- modules/products/src/lib/products.module.ts
+++ modules/products/src/lib/products.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiComponent } from 'modules/shared/ui';
 import { productsRoutes } from './lib.routes';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(productsRoutes)],
+  imports: [CommonModule, RouterModule.forChild(productsRoutes), UiComponent],
   declarations: [ProductListComponent],
 })
 export class ProductsModule {}
